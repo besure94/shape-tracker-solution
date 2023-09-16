4ff02f5e7181fa9cd1fb57d0c7f1c69930d34ed2
Brian Scherner <brianscherner@gmail.com>
2023-09-16 14:34:23 -0700

    Add _side3 get functionality and passing test.

--- ShapeTracker.Tests/ModelTests/RectangleTests.cs
+++ ShapeTracker.Tests/ModelTests/RectangleTests.cs
@@ -12,7 +12,7 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void RectangleConstructor_CreatesInstanceOfRectangle_Rectangle()
     {
-      Rectangle newRectangle = new Rectangle(6, 3);
+      Rectangle newRectangle = new Rectangle(6, 3, 6);
       Assert.AreEqual(typeof(Rectangle), newRectangle.GetType());
     }
 
@@ -20,7 +20,7 @@ namespace ShapeTracker.Tests
     public void GetSide1_ReturnsSide1_Int()
     {
       int length1 = 3;
-      Rectangle newRectangle = new Rectangle(length1, 6);
+      Rectangle newRectangle = new Rectangle(length1, 6, 3);
       int result = newRectangle.Side1;
       Assert.AreEqual(length1, result);
     }
@@ -28,7 +28,7 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void SetSide1_SetsValueOfSide1_Void()
     {
-      Rectangle newRectangle = new Rectangle(6, 3);
+      Rectangle newRectangle = new Rectangle(6, 3, 6);
       int newLength1 = 44;
       newRectangle.Side1 = newLength1;
       Assert.AreEqual(newLength1, newRectangle.Side1);
@@ -38,7 +38,7 @@ namespace ShapeTracker.Tests
     public void GetSide2_ReturnsSide2_Int()
     {
       int length2 = 3;
-      Rectangle newRectangle = new Rectangle(6, length2);
+      Rectangle newRectangle = new Rectangle(6, length2, 6);
       int result = newRectangle.Side2;
       Assert.AreEqual(length2, result);
     }
@@ -46,10 +46,19 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void SetSide2_SetsValueOfSide2_Void()
     {
-      Rectangle newRectangle = new Rectangle(3, 6);
+      Rectangle newRectangle = new Rectangle(3, 6, 6);
       int newLength2 = 6;
       newRectangle.Side2 = newLength2;
       Assert.AreEqual(newLength2, newRectangle.Side2);
+    }
+
+    [TestMethod]
+    public void GetSide3_ReturnsSide3_Int()
+    {
+      int length3 = 55;
+      Rectangle newRectangle = new Rectangle(3, 6, length3);
+      int result = newRectangle.GetSide3();
+      Assert.AreEqual(length3, result);
     }
 
   }
