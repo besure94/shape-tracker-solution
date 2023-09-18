5b6bc6fc8b058a1a8a10b80aefa6c2565301e8b9
Brian Scherner <brianscherner@gmail.com>
2023-09-18 11:51:35 -0700

    Start building functionality for rectangle calculation.

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -10,19 +10,44 @@ namespace ShapeTracker
     {
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to the Shape Tracker app!");
-      Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
-      Console.WriteLine("Please enter a number:");
-      string stringNumber1 = Console.ReadLine();
-      Console.WriteLine("Enter another number:");
-      string stringNumber2 = Console.ReadLine();
-      Console.WriteLine("Enter a third number:");
-      string stringNumber3 = Console.ReadLine();
-      int length1 = int.Parse(stringNumber1);
-      int length2 = int.Parse(stringNumber2);
-      int length3 = int.Parse(stringNumber3);
-      Triangle tri = new Triangle(length1, length2, length3);
-      ConfirmOrEditTriangle(tri);
-      Triangle.ClearAll();
+      Console.WriteLine("What type of shape would you like to calculate?");
+      Console.WriteLine("Please enter either 'triangle' or 'rectangle'.");
+      string inputtedShape = Console.ReadLine();
+      if (inputtedShape == "triangle" || inputtedShape == "Triangle")
+      {
+        Console.WriteLine("We'll calculate what type of triangle you have based off of the lengths of the triangle's 3 sides.");
+        Console.WriteLine("Please enter a number:");
+        string stringNumber1 = Console.ReadLine();
+        Console.WriteLine("Enter another number:");
+        string stringNumber2 = Console.ReadLine();
+        Console.WriteLine("Enter a third number:");
+        string stringNumber3 = Console.ReadLine();
+        int length1 = int.Parse(stringNumber1);
+        int length2 = int.Parse(stringNumber2);
+        int length3 = int.Parse(stringNumber3);
+        Triangle tri = new Triangle(length1, length2, length3);
+        ConfirmOrEditTriangle(tri);
+        Triangle.ClearAll();
+      }
+      else if (inputtedShape == "rectangle" || inputtedShape == "Rectangle")
+      {
+        Console.WriteLine("We'll calculate what type of rectangle you have based off of the lengths of the rectangle's 4 sides.");
+        Console.WriteLine("Please enter a number:");
+        string userNumber1 = Console.ReadLine();
+        Console.WriteLine("Enter another number:");
+        string userNumber2 = Console.ReadLine();
+        Console.WriteLine("Enter a third number:");
+        string userNumber3 = Console.ReadLine();
+        Console.WriteLine("Enter a fourth number:");
+        string userNumber4 = Console.ReadLine();
+        int rectangleLength1 = int.Parse(userNumber1);
+        int rectangleLength2 = int.Parse(userNumber2);
+        int rectangleLength3 = int.Parse(userNumber3);
+        int rectangleLength4 = int.Parse(userNumber4);
+        Rectangle rect = new Rectangle(rectangleLength1, rectangleLength2, rectangleLength3, rectangleLength4);
+        ConfirmOrEditRectangle(rect);
+        Rectangle.ClearAll();
+      }
     }
 
     static void ConfirmOrEditTriangle(Triangle tri)
@@ -31,7 +56,7 @@ namespace ShapeTracker
       Console.WriteLine($"Side 1 has a length of {tri.Side1}.");
       Console.WriteLine($"Side 2 has a length of {tri.Side2}.");
       Console.WriteLine($"Side 3 has a length of {tri.GetSide3()}.");
-      Console.WriteLine("Is that correct? Enter 'yes' to proceed, or 'no' to re-enter the triangle's sides");
+      Console.WriteLine("Is that correct? Enter 'yes' to proceed, or 'no' to re-enter the triangle's sides.");
       string userInput = Console.ReadLine();
       if (userInput == "yes")
       {
@@ -95,6 +120,21 @@ namespace ShapeTracker
       else
       {
         Console.WriteLine("Goodbye!");
+      }
+    }
+
+    static void ConfirmOrEditRectangle(Rectangle rect)
+    {
+      Console.WriteLine("Please confirm that you entered in your rectangle correctly:");
+      Console.WriteLine($"Side 1 has a length of {rect.Side1}.");
+      Console.WriteLine($"Side 2 has a length of {rect.Side2}.");
+      Console.WriteLine($"Side 3 has a length of {rect.GetSide3()}.");
+      Console.WriteLine($"Side 4 has a length of {rect.GetSide4()}.");
+      Console.WriteLine("Is that correct? Enter 'yes' to proceed, or 'no' to re-enter the rectangle's sides".);
+      string userString = Console.ReadLine();
+      if (userString == "yes" || userString == "Yes")
+      {
+        CheckRectangleType(rect);
       }
     }
   }
